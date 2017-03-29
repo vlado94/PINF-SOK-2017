306e3b575c6a1ad4937c231553fa80327e9fae8e
vlado94 <vladimir94@gmail.com>
2017-03-29 18:30:00 +0200

    --

--- Banka/src/main/java/app/billPersonal/BillPersonal.java
+++ Banka/src/main/java/app/billPersonal/BillPersonal.java
@@ -1,5 +1,7 @@
 package app.billPersonal;
 
+import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -27,9 +29,15 @@ public class BillPersonal {
 	@Column(unique = true)
 	private String billNumber;
 	
+	@NotBlank
+	@Column
+	private boolean active;
+	
+	@Column
+	private Date activationDate;	
+	
 	@JsonIgnore
 	@ManyToOne			
 	@JoinColumn(name = "CLIENT_PERSONAL_ID")
-	private ClientPersonal clientPersonal;
-	
+	private ClientPersonal clientPersonal;	
 }
